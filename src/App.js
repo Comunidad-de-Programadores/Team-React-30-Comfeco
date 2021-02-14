9ab2c85cea4a5a8ec14ddb73a00688dddc6ff1c1
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-02-14 11:51:49 -0500

    Router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import PrivateRoute from './components/PrivateRoute';
+
+import Auth from './views/Auth';
+import Home from './views/Home';
+
 function App() {
-  return <h1>AppComponent</h1>;
+  return (
+    <>
+      <Switch>
+        <Route exact path="/" component={Auth} />
+        <PrivateRoute path="/home" component={Home} />
+      </Switch>
+    </>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import AuthProvider from './context/auth';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <Router>
+        <App />
+      </Router>
+    </AuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
