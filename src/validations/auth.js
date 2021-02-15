43835514218322909a416573fa879c04f22fafe3
Jhonny Quispe <jhonny.quispejl@gmail.com>
2021-02-15 17:59:07 -0500

    registro de usuario y mostrar mensajes

--- src/validations/auth.js
+++ src/validations/auth.js
@@ -1,31 +1,33 @@
 import * as yup from 'yup';
 
-export const signInSchema = yup.object().shape({
-    email: yup
-        .string()
-        .email('Correo no válido')
-        .required('El correo es obligatorio'),
-    password: yup
-        .string()
-        .required('La contraseña es obligatoria')
+export const loginSchema = yup.object().shape({
+  email: yup
+    .string()
+    .email('Correo no válido')
+    .required('El correo es obligatorio'),
+  password: yup.string().required('La contraseña es obligatoria'),
 });
 
-export const signUpSchema = yup.object().shape({
-    nick: yup
-        .string()
-        .required('El nick es obligatorio'),
-    email: yup
-        .string()
-        .email('Correo no válido')
-        .required('El correo es obligatorio'),
-    password: yup
-        .string()
-        .required('La contraseña es obligatoria')
-        .min(8, 'La contraseña debe tener 8 caracteres como mínimo')
-        .matches(/.*[0-9].*/, 'La contraseña debe tener al menos un número')
-        .matches(/.*[A-Z].*/, 'La contraseña debe tener al menos un caracter en mayuscula')
-        .matches(/.*[a-z].*/, 'La contraseña debe tener al menos un caracter en minuscula'),
-    passwordConfirmation: yup
-        .string()
-        .oneOf([yup.ref('password'), null], 'Las contraseñas no coinciden')
+export const registerSchema = yup.object().shape({
+  registerNick: yup.string().required('El nick es obligatorio'),
+  registerEmail: yup
+    .string()
+    .email('Correo no válido')
+    .required('El correo es obligatorio'),
+  registerPassword: yup
+    .string()
+    .required('La contraseña es obligatoria')
+    .min(8, 'La contraseña debe tener 8 caracteres como mínimo')
+    .matches(/.*[0-9].*/, 'La contraseña debe tener al menos un número')
+    .matches(
+      /.*[A-Z].*/,
+      'La contraseña debe tener al menos un caracter en mayuscula'
+    )
+    .matches(
+      /.*[a-z].*/,
+      'La contraseña debe tener al menos un caracter en minuscula'
+    ),
+  registerRepeat: yup
+    .string()
+    .oneOf([yup.ref('registerPassword'), null], 'Las contraseñas no coinciden'),
 });
