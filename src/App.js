a4800086ad4bec2ca77b8bf7e9c54960c0ceddfd
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-02-15 15:21:20 -0500

    Actualizando App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
-import PrivateRoute from './components/PrivateRoute';
-
-import Auth from './views/Auth';
-import Home from './views/Home';
 
 function App() {
   return (
-    <>
-      <Switch>
-        <Route exact path="/" component={Auth} />
-        <PrivateRoute path="/home" component={Home} />
-      </Switch>
-    </>
+    <div>App</div>
   );
 }
 
