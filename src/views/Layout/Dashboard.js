6ea2d4d6979009996b324a57e40587ab7441775f
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-02-17 11:49:59 -0500

    Añadiendo Header al Layout

--- src/views/Layout/Dashboard.js
+++ src/views/Layout/Dashboard.js
@@ -1,7 +1,9 @@
 import Footer from '../../components/Layout/Footer/Footer';
+import { Header } from '../../components/Layout/Header/Header';
 
 const Layout = ({ children }) => (
   <>
+    <Header />
     {children}
     <Footer />
   </>
