bbeda1b512382b9f86b82cd4b0b715f2f7cfc360
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-02-17 16:18:36 -0500

    Cambiando estructura

--- src/views/Auth/PasswordReset/PasswordReset.js
+++ src/views/Auth/PasswordReset/PasswordReset.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { passwordResetSchema as schema } from '../../validations/auth';
+import { passwordResetSchema as schema } from '../../../validations/auth';
 
 export default function PasswordReset() {
   const { register, handleSubmit, errors } = useForm({
