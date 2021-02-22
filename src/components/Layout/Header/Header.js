2e0fc6df8814711e831e72713020268ea11e61bd
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-02-22 15:30:59 -0500

    Link a login en recuperación de contraseña

--- src/components/Layout/Header/Header.js
+++ src/components/Layout/Header/Header.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import './header.css';
 import Logo from '../../../assets/images/logo_horizontal.png';
 
-export const Header = () => (
-  <header className="header">
-    <Link to="/" className="header-link">
-      <img src={Logo} alt="Logo Confeco" />
-    </Link>
+export const Header = () => {
+  const location = useLocation();
 
-    <div className="header-message">&lt;&gt;HolaMundo&lt;/&gt;</div>
-  </header>
-);
+  return (
+    <header className="header">
+      <Link to="/" className="header-link">
+        <img src={Logo} alt="Logo Confeco" />
+      </Link>
+
+      {location.pathname === '/auth' ? (
+        <div className="header-message">&lt;&gt;HolaMundo&lt;/&gt;</div>
+      ) : (
+        ''
+      )}
+      {location.pathname === '/password-reset' ? (
+        <Link to="/" className="link">Regresar al login</Link>
+      ) : (
+        ''
+      )}
+    </header>
+  );
+};
