d935441880bda9f1a60e3bf26c7622d1ff86f133
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-02-22 13:56:43 -0500

    Descripción de Home

--- src/components/Layout/Timer/Timer.js
+++ src/components/Layout/Timer/Timer.js
@@ -2,28 +2,41 @@ import React from 'react';
 
 import './timer.css';
 
-export const Timer = () => (
-  <section className="timer">
-    <div className="timer-content">
-      <div className="timer-section day">
-        <h3>365</h3>
-        <p>Dias</p>
-      </div>
+const decomposeTime = (seconds) => {
+  const days = Math.floor(seconds / (3600 * 24));
+  const hours = Math.floor((seconds % (3600 * 24)) / 3600);
+  const minutes = Math.floor((seconds % 3600) / 60);
+  const sec = Math.floor(seconds % 60);
 
-      <div className="timer-section hour">
-        <h3>00</h3>
-        <p>Horas</p>
-      </div>
+  return { days, hours, minutes, sec };
+};
 
-      <div className="timer-section minute">
-        <h3>00</h3>
-        <p>Minutos</p>
-      </div>
+export const Timer = ({ seconds }) => {
+  const { days, hours, minutes, sec } = decomposeTime(seconds);
 
-      <div className="timer-section seconds">
-        <h3>00</h3>
-        <p>Segundos</p>
+  return (
+    <section className="timer">
+      <div className="timer-content">
+        <div className="timer-section day">
+          <h3>{days < 10 ? `0${days}` : days}</h3>
+          <p>Dias</p>
+        </div>
+
+        <div className="timer-section hour">
+          <h3>{hours < 10 ? `0${hours}` : hours}</h3>
+          <p>Horas</p>
+        </div>
+
+        <div className="timer-section minute">
+          <h3>{minutes < 10 ? `0${minutes}` : minutes}</h3>
+          <p>Minutos</p>
+        </div>
+
+        <div className="timer-section seconds">
+          <h3>{sec < 10 ? `0${sec}` : sec}</h3>
+          <p>Segundos</p>
+        </div>
       </div>
-    </div>
-  </section>
-);
+    </section>
+  );
+};
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,4 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import './home.css';
 import { Timer } from '../../components/Layout/Timer/Timer';
 
-export const Home = () => <Timer />;
+export const Home = () => {
+  const [timeLeft, setTimeLeft] = useState(3.154e7);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newTimeLeft = timeLeft - 1;
+      setTimeLeft(newTimeLeft);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  return (
+    <>
+      <div className="home">
+        <div className="home-description">
+          <p className="home-description title">
+            Bienvenido a Comunity Fest and Code
+          </p>
+          <p className="home-description subtitle">
+            ¡Conoce gente, aprende y gana!
+          </p>
+          <p className="home-description text">
+            La próxima edición regresa en el 2022, en la cual se planea
+            involucrar a todos los programadores independientemente del area de
+            conocimiento que se encuentre, todo con un mismo propósito, aprender
+            en comunidad.
+          </p>
+        </div>
+        <section className="home-grid">
+          <div>Col1</div>
+          <div>Col2</div>
+        </section>
+      </div>
+      <Timer seconds={timeLeft} />
+    </>
+  );
+};
