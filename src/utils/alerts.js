3300a9c376f3bd0f09cd56d4a7b019a29a231e92
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-02-25 15:52:16 -0500

    Arreglando login

--- src/utils/alerts.js
+++ src/utils/alerts.js
@@ -1,19 +1,17 @@
 import Swal from 'sweetalert2';
 
-export const errorAlert = (error) => {
+export const errorAlert = (error) =>
   Swal.fire({
     title: 'Que mal!',
     text: error.response.data.message[0].messages[0].message,
     icon: 'error',
     confirmButtonText: 'Aceptar',
   });
-};
 
-export const messageAlert = (icon, text) => {
+export const messageAlert = (icon, text) =>
   Swal.fire({
     title: 'Genial!',
     text,
     icon,
     confirmButtonText: 'Aceptar',
   });
-};
