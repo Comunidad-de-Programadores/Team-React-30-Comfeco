165bd10c42c2d6af1393f2746b80fb7adb31d9cd
gabriel <gbordesc@uft.edu>
2021-02-25 22:25:01 -0300

    Buen color: :

--- src/views/Error/Error.js
+++ src/views/Error/Error.js
@@ -4,8 +4,11 @@ import './Error.css'
 const Error = () =>
 (
   <>
-    <div className="error ">
-      <h1>Error 404</h1> 
+    <div className="error">
+
+      <h1>404</h1>
+      <p>Oops! Something is wrong.</p>
+      <a className="buttonx" href="/"><i className="icon-home" /> Go back in initial page, is better.</a>
     </div>
   </>
 );
