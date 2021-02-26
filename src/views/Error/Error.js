6bdd1e9ccb6c1492ab1ac300ff63157f42e835c8
gabriel <gbordesc@uft.edu>
2021-02-25 22:47:07 -0300

    error page agregado

--- src/views/Error/Error.js
+++ src/views/Error/Error.js
@@ -7,8 +7,8 @@ const Error = () =>
     <div className="error">
 
       <h1>404</h1>
-      <p>Oops! Something is wrong.</p>
-      <a className="buttonx" href="/"><i className="icon-home" /> Go back in initial page, is better.</a>
+      <p>No encontramos lo que buscabas.</p>
+      <a className="buttonx" href="/"><i className="icon-home" /> Haz click aca para regresar!</a>
     </div>
   </>
 );
