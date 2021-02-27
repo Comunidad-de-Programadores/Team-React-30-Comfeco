d0b6e9c5e1710477c75836d2c9e2ad71c1d72498
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-02-27 15:10:43 -0500

    Añadiendo token en request

--- src/utils/http.js
+++ src/utils/http.js
@@ -2,9 +2,25 @@ import axios from 'axios';
 
 export const baseURL = 'https://team-react-30-confeco-heroku.herokuapp.com/';
 
-export default axios.create({
+const instance = axios.create({
   baseURL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    const userData = sessionStorage.getItem('userData');
+    if (userData) {
+      const { token } = userData;
+      // eslint-disable-next-line no-param-reassign
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
+
+export default instance;
