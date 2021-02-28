83c685e55ff9aa3701552ca52b812c20e7c31492
gabriel <gbordesc@uft.edu>
2021-02-28 19:10:16 -0300

    Comente el codigo para mayor entendimiento del manejo del error

--- src/utils/alerts.js
+++ src/utils/alerts.js
@@ -5,7 +5,7 @@ import Swal from 'sweetalert2';
 export const errorAlert = (error) =>
   Swal.fire({
     title: 'Que mal!',
-    text: error===false ? 'Ocurrio un error por parte del servidor':error.response.data.message[0].messages[0].message,
+    text: error===false ? 'Ocurrio un error por parte del servidor':error.response.data.message[0].messages[0].message,// Aca se evalua, si error=false, significa que el servidor envio un undefined, caso contrario se muestra el mensaje del servidor en una notificacion
     icon: 'error',
     confirmButtonText: 'Aceptar',
   });
