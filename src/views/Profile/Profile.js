c88d4c2eb1eebcbf5d29283323f130e1b5a85b23
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-03-01 17:17:14 -0500

    Removiendo formulario de vista

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,12 +1,10 @@
 import React from 'react'
 import './profile.css'
 
-import EditForm from '../../components/Profile/EditForm/EditForm';
-
 export default function Profile() {
     return (
       <div>
-        <EditForm />
+        Profile
       </div>
     )
 }
