7497bd4d1894c3d81e50b1b513171e77f715e070
Mario Sanchez <mgsanchezdev@gmail.com>
2021-03-02 18:52:24 -0300

    Borrando compoenente de la vista perfil

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,11 +1,9 @@
 import React from 'react'
-import EditForm from '../../components/Profile/EditForm/EditForm';
+
 import './profile.css'
 
 export default function Profile() {
     return (
-      <div>
-        <EditForm />
-      </div>
+      <div />
     )
 }
