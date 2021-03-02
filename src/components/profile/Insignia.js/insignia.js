8dbfa6755faa3a20c2785d04723b5ac3b3da7b66
marco <marcoantonio1708@gmail.com>
2021-03-02 22:55:57 +0100

    cambios de sintaxis

--- src/components/profile/Insignia.js/insignia.js
+++ src/components/profile/Insignia.js/insignia.js
@@ -1,30 +1,31 @@
-import './Insignia.css'
+import './Insignia.css';
 
-import React from 'react'
+import React from 'react';
 
 export const Insignia = () => {
-    return (
-        <>
-        <div id="content">
+  return (
+    <>
+      <div id="content">
         <div id="insignias">
           <h6 id="insignias-title">Insignias</h6>
           <div id="insignias-image">
-          <img id="" src="https://via.placeholder.com/90x90"></img>
-          <img id="" src="https://via.placeholder.com/90x90"></img>
-          <img id="" src="https://via.placeholder.com/90x90"></img>
-          <img id="" src="https://via.placeholder.com/90x90"></img>
+            <img id="" src="https://via.placeholder.com/90x90"></img>
+            <img id="" src="https://via.placeholder.com/90x90"></img>
+            <img id="" src="https://via.placeholder.com/90x90"></img>
+            <img id="" src="https://via.placeholder.com/90x90"></img>
           </div>
         </div>
-        
+
         <h5 id="activity-title">Actividad Reciente</h5>
         <div id="activity">
-          
-          <p id="activity-text">Te has Unido al evento Community Fest and Code</p>
-          <div id="activity-calendar"><img src="calendar.svg"></img></div>
-        
+          <p id="activity-text">
+            Te has Unido al evento Community Fest and Code
+          </p>
+          <div id="activity-calendar">
+            <img src="calendar.svg"></img>
+          </div>
         </div>
-        </div>
-        </>
-      );
-}
-
+      </div>
+    </>
+  );
+};
