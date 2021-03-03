5df1ed8126468ab6f555d997fb58f3709e361589
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-03-03 15:07:12 -0500

    Cambiando secciones con botones de submenu

--- src/components/Profile/SubMenu/SubMenu.js
+++ src/components/Profile/SubMenu/SubMenu.js
@@ -8,18 +8,20 @@ import Trophy from '../../Icons/Trophy';
 import Calendar from '../../Icons/Calendar';
 import ArrowDown from '../../Icons/Arrows/ArrowDown';
 import ArrowUp from '../../Icons/Arrows/ArrowUp';
+import { useSelector } from '../../../context/selector';
 
 export const SubMenu = () => {
+  const { goTo } = useSelector();
   const [isOpen, setIsOpen] = useState(false);
 
   const open = () => {
-    setIsOpen(value => !value);
+    setIsOpen((value) => !value);
   };
 
   const classes = ['menu'];
 
   if (isOpen) {
-      classes.push('open');
+    classes.push('open');
   }
 
   return (
@@ -27,30 +29,36 @@ export const SubMenu = () => {
       <div className="submenu">
         <div className="toggle-submenu">
           <span>Menú</span>
-          <span className="toggle-submenu-button" role="button" onClick={open} tabIndex={0} onKeyDown={open}>
-            { !isOpen ? <ArrowDown /> : <ArrowUp /> }
+          <span
+            className="toggle-submenu-button"
+            role="button"
+            onClick={open}
+            tabIndex={0}
+            onKeyDown={open}
+          >
+            {!isOpen ? <ArrowDown /> : <ArrowUp />}
           </span>
         </div>
         <div className={classes.join(' ')}>
-          <button id="profile" type="button" className="submenu-button">
+          <button id="profile" type="button" className="submenu-button" onClick={() => goTo('profile-start')}>
             <span className="submenu-button-icon">
               <Person />
             </span>
             <span>Mi Perfil</span>
           </button>
-          <button id="trophies" type="button" className="submenu-button">
+          <button id="trophies" type="button" className="submenu-button" onClick={() => goTo('medals')}>
             <span className="submenu-button-icon">
               <Trophy />
             </span>
             <span>Insignias</span>
           </button>
-          <button id="groups" type="button" className="submenu-button">
+          <button id="groups" type="button" className="submenu-button" onClick={() => goTo('groups')}>
             <span className="submenu-button-icon">
               <People />
             </span>
             <span>Grupos</span>
           </button>
-          <button id="events" type="button" className="submenu-button">
+          <button id="events" type="button" className="submenu-button" onClick={() => goTo('events')}>
             <span className="submenu-button-icon">
               <Calendar />
             </span>
