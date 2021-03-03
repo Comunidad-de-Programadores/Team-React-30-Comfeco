c12fcbbc7b446df311de59e0255db58809ae4319
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-03-03 14:26:50 -0500

    Modificando submenu

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import './profile.css'
-import {SubMenu} from '../../components/SubMenu/SubMenu'
+import {SubMenu} from '../../components/Profile/SubMenu/SubMenu'
 
 export default function Profile() {
     return (
