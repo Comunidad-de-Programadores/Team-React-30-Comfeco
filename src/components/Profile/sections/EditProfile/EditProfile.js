c102aad1afe5a86a6268bd1b5a824d1e13ef462a
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-03-03 14:53:50 -0500

    Añadiendo secciones

---
+++ src/components/Profile/sections/EditProfile/EditProfile.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const EditProfile = () => <div>EditProfile component</div>;
+
+export default EditProfile;
---
+++ src/components/Profile/sections/Groups/Groups.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Groups = () => <div>Groups component</div>;
+
+export default Groups;
