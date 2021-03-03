ada0883bf0d4beb4b4ced6eb1acf48a47dd6f17b
rodsil01 <47982767+rodsil01@users.noreply.github.com>
2021-03-03 15:00:08 -0500

    Usando componente en la vista Profile

--- src/views/Profile/Profile.js
+++ src/views/Profile/Profile.js
@@ -2,12 +2,26 @@ import React from 'react'
 
 import './profile.css'
 import {SubMenu} from '../../components/SubMenu/SubMenu'
+import SelectorProvider from '../../context/selector'
+import ViewSelector from '../../components/ViewSelector/ViewSelector'
+import ViewItem from '../../components/ViewSelector/ViewItem/ViewItem'
+import ProfileStart from '../../components/Profile/sections/ProfileStart/ProfileStart'
+import EditProfile from '../../components/Profile/sections/EditProfile/EditProfile'
+import Groups from '../../components/Profile/sections/Groups/Groups'
+import Events from '../../components/Profile/sections/Events/Events'
+import Medals from '../../components/Profile/sections/Medals/Medals'
 
 export default function Profile() {
     return (
-      <>
+      <SelectorProvider>
         <SubMenu />
-      </>
-
+        <ViewSelector initialView="profile-start">
+          <ViewItem name="profile-start" component={ProfileStart} />
+          <ViewItem name="edit-profile" component={EditProfile} />
+          <ViewItem name="groups" component={Groups} />
+          <ViewItem name="events" component={Events} />
+          <ViewItem name="medals" component={Medals} />
+        </ViewSelector>
+      </SelectorProvider>
     )
 }
