57d765f68ec19dc897bd66747a633bd09ab7349c
Mario Sanchez <mgsanchezdev@gmail.com>
2021-03-17 21:01:22 -0300

    Modificar link eventos

--- src/components/Profile/EventsBox/EventsBox.js
+++ src/components/Profile/EventsBox/EventsBox.js
@@ -1,15 +1,16 @@
 import './EventsBox.css';
-
 import React from 'react';
+import {useSelector} from '../../../context/selector';
 
 export default function EventsBox() {
+  const { goTo } = useSelector();
   return (
     <div id="Events-box">
       <div id="Events-title">
         <h5 id="">Eventos de tu interes</h5>
-        <a id="Events-more" href="/">
+        <button type='button' id="Events-more" onClick={()=>goTo('events')}>
           Ver mas
-        </a>
+        </button>
       </div>
     </div>
   );
